36a26b6c1846702841fda46f3a5944a50b4d9557
Tim Riley <tim@openmonkey.com>
2018-02-24 21:39:07 +1100

    Add unique indexes on article path and permalink

--- lib/database/relations/articles.rb
+++ lib/database/relations/articles.rb
@@ -3,11 +3,16 @@ module Database
     class Articles < ROM::Relation[:sql]
       schema :articles do
         attribute :id, Types::Serial
-        attribute :path, Types::String # TODO: make this the primary key
+        attribute :path, Types::String
         attribute :title, Types::String
         attribute :permalink, Types::String
         attribute :published_at, Types::Time.optional
         attribute :body, Types::String
+
+        indexes do
+          index :path, name: :unique_path, unique: true
+          index :permalink, name: :unique_permalink, unique: true
+        end
       end
     end
   end
