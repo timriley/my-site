b833edc466af2a14ece064867c9827267770047e
Tim Riley <tim@openmonkey.com>
2018-03-07 13:29:38 +1100

    Add site-wide info (name, author, URL) to settings

--- system/boot/settings.rb
+++ system/boot/settings.rb
@@ -6,5 +6,9 @@ Site::Container.boot :settings, from: :system do
   settings do
     key :import_dir, Site::Types::Strict::String
     key :export_dir, Site::Types::Strict::String
+
+    key :site_name, Site::Types::Strict::String
+    key :site_author, Site::Types::Strict::String
+    key :site_url, Site::Types::Strict::String
   end
 end
