b8a2bcfb8971e7977c72eddf63bb4bdcc66adea5
Tim Riley <tim@openmonkey.com>
2018-11-22 20:46:45 +1100

    Fix bug inferring entry type from file name

--- lib/site/importers/files.rb
+++ lib/site/importers/files.rb
@@ -38,13 +38,15 @@ module Site
       private
 
       def parse_data(file_name, dir:)
+        relative_file_path = Pathname(file_name).relative_path_from(Pathname(dir)).to_s
+
         parsed_file = FrontMatterParser::Parser.parse_file(file_name, loader: FRONT_MATTER_LOADER)
         data = Functions[:symbolize][parsed_file.front_matter]
-        type = data[:type] || infer_type_from_file_name(file_name)
+        type = data[:type] || infer_type_from_file_name(relative_file_path)
 
         data.merge(
           type: type,
-          path: Pathname(file_name).relative_path_from(Pathname(dir)).to_s,
+          path: relative_file_path,
           body: parsed_file.content,
         )
       end
