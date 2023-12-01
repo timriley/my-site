36d29e5c347d814f5811355e6509280b4416102c
Tim Riley <tim@riley.id.au>
2023-12-02 06:56:09 +1100

    Remove commented view code

--- app/view.rb
+++ app/view.rb
@@ -6,16 +6,5 @@ require "slim"
 
 module Site
   class View < Hanami::View
-    # config.paths = [Hanami.app.root.join("templates")]
-    # config.default_context = Hanami.app["view.context"]
-    # config.layout = "site"
-
-    # def call(context: nil, **input)
-    #   # Always provide a new context object, so we don't carry over state
-    #   # between renderings
-    #   context ||= config.default_context.with({})
-
-    #   super(context: context, **input)
-    # end
   end
 end
