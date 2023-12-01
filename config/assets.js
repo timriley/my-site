8c2b7c9d6d60571b7064a503a940d6d6eaac3a05
Tim Riley <tim@riley.id.au>
2023-12-01 17:07:04 +1100

    Up esbuild log level

--- config/assets.js
+++ config/assets.js
@@ -1,3 +1,9 @@
 import * as assets from "hanami-assets";
 
-await assets.run();
+await assets.run({
+  esbuildOptionsFn: (args, esbuildOptions) => {
+    esbuildOptions.logLevel = "info";
+
+    return esbuildOptions;
+  }
+});
